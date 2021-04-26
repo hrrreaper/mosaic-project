83182fe567df1fc1e23280ad3a89a12e464d4011
Heather Reaper <heather.reaper@gmail.com>
2021-04-26 17:13:32 -0400

    fixed server file

--- server/index.js
+++ server/index.js
@@ -11,26 +11,29 @@ const {
   setUsers,
 } = require('./handlers');
 
-express()
-  .use(express.json())
-  .use(morgan("tiny"))
 
-  .get('/beers', getAllBeers)
-  .get('/beer/:_id', getOneBeer)
-  .delete('/beer/:_id', deleteBeer)
-  .patch('/update/:_id', updateBeer)
-  .post('/add/beer', addBeer)
-  .post('/api/v1/auth/google', setUsers)
+const app = express()
+  
+  app.use(express.json())
+  app.use(morgan("tiny"))
+
+  app.get('/beers', getAllBeers)
+  app.get('/beer/:_id', getOneBeer)
+  app.delete('/beer/:_id', deleteBeer)
+  app.patch('/update/:_id', updateBeer)
+  app.post('/add/beer', addBeer)
+  app.post('/api/v1/auth/google', setUsers)
 
 
-  .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
+  app.use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  });
+  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
   }
-
-  app.listen(process.env.PORT || 8000, () => console.log(`🌍 Listening on port 8000`));
+  
+app.listen(process.env.PORT || 8000, () => console.log(`🌍 Listening on port 8000`));
